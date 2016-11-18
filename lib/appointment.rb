82c02ff030f61e9acec2845ccbcf57f31d622d6a
richo225 <richo225@gmail.com>
2016-11-18 20:33:14 +0000

    Validates requested time and updates tests

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -11,24 +11,37 @@ class Appointment
   def get_slots
     json = File.read("lib/availability_slots.json")
     @slots = JSON.parse(json)["availability_slots"]
-  end    
+  end
+
+  def check_validity
+    if valid?
+      check_availability
+    else
+      raise "Please pick a time between 08:00:00 and 15:00:00"
+    end
+  end
 
   def check_availability
     if available?
       "Appointment confirmed at #{@requested_time}"
     else
-      "Please pick another time"
+      raise "Appointment unavailable. Please pick an earlier time"
     end
   end
 
   private
 
+  def valid?
+    requested_time.between?("08:00:00","15:00:00")
+  end
+
   def available?
-    slots.any? {|slot| slot["time"] == requested_time}
+    slots.any? {|slot| slot["time"] >= requested_time}
   end
 
   # Check if exact time is available ie. "booked" => false or deleted
   # If it is, run booking method and either delete that appointment or add a key "booked" => true
+    # Write this to the JSON file
   # If it isn't, find nearest value and repeat
 
 end
--- spec/appointment_spec.rb
+++ spec/appointment_spec.rb
@@ -9,19 +9,33 @@ describe Appointment do
     expect(appointment.get_slots).to be_an Array
   end
 
-  context "slot is available" do
-    it "returns confirmation message if time available" do
+  context "requested time is invalid" do
+    invalid = "Please pick a time between 08:00:00 and 15:00:00"
+
+    it "raises an error if time is before 08:00:00" do
+      appointment = Appointment.new("07:30:00")
+      expect{appointment.check_validity}.to raise_error invalid
+    end
+
+    it "raises an error if time is after 15:00:00" do
+      appointment = Appointment.new("15:30:00")
+      expect{appointment.check_validity}.to raise_error invalid
+    end
+  end
+
+  context "requested time is available" do
+    it "returns a confirmation message if time is available" do
       message = "Appointment confirmed at 08:00:00"
       expect(appointment.check_availability).to eq message
     end
   end
 
-  context "slot is not available" do
+  context "requested time is not available" do
     let(:appointment){Appointment.new("22:00:00")}
 
-    it "returns error message if time unavailable" do
-      message = "Please pick another time"
-      expect(appointment.check_availability).to eq message
+    it "asks for another time if unavailable" do
+      unavailable = "Appointment unavailable. Please pick an earlier time"
+      expect{appointment.check_availability}.to raise_error unavailable
     end
   end
 
