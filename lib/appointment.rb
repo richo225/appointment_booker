b2c0e0377b4f598054a70972bfc83553c210fcef
richo225 <richo225@gmail.com>
2016-11-18 18:29:56 +0000

    First version for perfect time match

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,15 +1,34 @@
 require "json"
 
 class Appointment
+  attr_reader :slots, :requested_time
 
-  def initialize(request)
-    @requested_time = request
+  def initialize(time)
+    @requested_time = time
     get_slots
   end
 
   def get_slots
     json = File.read("lib/availability_slots.json")
     @slots = JSON.parse(json)["availability_slots"]
+  end    
+
+  def check_availability
+    if available?
+      "Appointment confirmed at #{@requested_time}"
+    else
+      "Please pick another time"
+    end
   end
+
+  private
+
+  def available?
+    slots.any? {|slot| slot["time"] == requested_time}
+  end
+
+  # Check if exact time is available ie. "booked" => false or deleted
+  # If it is, run booking method and either delete that appointment or add a key "booked" => true
+  # If it isn't, find nearest value and repeat
 
 end
--- spec/appointment_spec.rb
+++ spec/appointment_spec.rb
@@ -2,11 +2,27 @@ require "appointment"
 
 describe Appointment do
 
-  let(:appointment){Appointment.new("8:00")}
+  let(:appointment){Appointment.new("08:00:00")}
 
   it "correctly parses JSON to an array" do
     expect{appointment.get_slots}.to_not raise_error
     expect(appointment.get_slots).to be_an Array
+  end
+
+  context "slot is available" do
+    it "returns confirmation message if time available" do
+      message = "Appointment confirmed at 08:00:00"
+      expect(appointment.check_availability).to eq message
+    end
+  end
+
+  context "slot is not available" do
+    let(:appointment){Appointment.new("22:00:00")}
+
+    it "returns error message if time unavailable" do
+      message = "Please pick another time"
+      expect(appointment.check_availability).to eq message
+    end
   end
 
 end
